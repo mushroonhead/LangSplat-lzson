Fixed bugs in transmitting opacity scaling to rendering function
- Prior opacity scaling function not passed into rendering model, fixed it and updated examples

open_lang_finder.py
@@ -49,15 +49,15 @@ if __name__ == "__main__":
         pipeline_params=pipeline_params, device=device,
         args=args
     ).to(device)
-    # relevancy_pipeline = LangSplatRelevancyPipeline(
-    #     root_pipeline=root_pipeline,
-    #     device = device
+    relevancy_pipeline = LangSplatRelevancyPipeline(
+        root_pipeline=root_pipeline,
+        device = device
+    ).to(device)
+    # relevancy_pipeline = TestPipeline(
+    #     root_pipeline=root_pipeline
     # ).to(device)
-    # # relevancy_pipeline = TestPipeline(
-    # #     root_pipeline=root_pipeline
-    # # ).to(device)
 
-    # queries = ['pikachu','gundam'] #N queries
+    queries = ['pikachu','gundam'] #N queries
 
     # # render with no grad
     # cam = root_pipeline.scene.getTrainCameras()[0] # temporary get 1
@@ -65,21 +65,22 @@ if __name__ == "__main__":
     # t = torch.tensor(cam.T[None,...], device=device, dtype=torch.float32, requires_grad=True)
     # opa_scaling = torch.randn_like(root_pipeline.gaussian.get_xyz[...,-1], requires_grad=True)
     # opa_scaling_norm = torch.distributions.Normal(opa_scaling.mean(), opa_scaling.cov().clamp(min=1e-3)).cdf(opa_scaling)[...,None]
-    # valid_map = relevancy_pipeline(queries[0], R, t, #opa_scaling=opa_scaling_norm, 
+    # valid_map = relevancy_pipeline(queries[0], R, t, 
+    #                                opa_scaling=opa_scaling_norm, 
     #                                pipeline_params=pipeline_params)
-    # # plt.imshow(valid_map[0,0].detach().cpu().numpy())
-    # # plt.show()
-    # num_imgs = valid_map.shape[0]
-    # row = int(math.ceil(math.sqrt(num_imgs)))
-    # fig, axs = plt.subplots(row, row, figsize=(row*6, row*6))
-    # for i, img in enumerate(valid_map):
-    #     j, k = divmod(i, row)
-    #     axs[j,k].imshow(valid_map[i].detach().cpu().numpy())
+    # plt.imshow(valid_map[0].detach().cpu().numpy())
     # plt.show()
-    # valid_map.sum().backward(inputs=(R,t,opa_scaling)) # temp test
-    # print('R grad:', R.grad)
-    # print('t grad:', t.grad)
-    # print('op grad:', opa_scaling)
+    # # num_imgs = valid_map.shape[0]
+    # # row = int(math.ceil(math.sqrt(num_imgs)))
+    # # fig, axs = plt.subplots(row, row, figsize=(row*6, row*6))
+    # # for i, img in enumerate(valid_map):
+    # #     j, k = divmod(i, row)
+    # #     axs[j,k].imshow(valid_map[i].detach().cpu().numpy())
+    # # plt.show()
+    # # valid_map.sum().backward(inputs=(R,t,opa_scaling)) # temp test
+    # # print('R grad:', R.grad)
+    # # print('t grad:', t.grad)
+    # # print('op grad:', opa_scaling)
     # pass
 
     query = 'pikachu'
@@ -89,7 +90,7 @@ if __name__ == "__main__":
     tensor_kwargs={'device':device,'dtype':torch.float32}
 
     num_iter = 250
-    jitter_sigma = 10.
+    jitter_sigma = 100.
     render_cycle = 5
     render_dir = './output/lzson'
 

relevancy_methods.py
@@ -57,7 +57,8 @@ class TestPipeline(torch.nn.Module):
         # return self.cos_similarity_comp(query, R, t, opa_scaling=opa_scaling,
         #                                 pipeline_params=pipeline_params, override_color=override_color,
         #                                 decode_batchsize=decode_batchsize)
-        return self.render_random_views(5, pipeline_params, override_color)
+        return self.render_random_views(5, opa_scaling, 
+                                        pipeline_params=pipeline_params, override_color=override_color)
     
     def cos_similarity_comp(self, query: str, R: torch.Tensor, t: torch.Tensor,
                             pipeline_params: PipelineParams,
@@ -146,6 +147,7 @@ class TestPipeline(torch.nn.Module):
         # # return view_rotations, (view_rotations @ t[...,None]).squeeze(-1)
     
     def render_random_views(self, num_views,
+                            opa_scaling: Optional[torch.Tensor],
                             pipeline_params: PipelineParams,
                             override_color=None):
         Rs, ts = self.random_select_views(num_views)
@@ -153,7 +155,7 @@ class TestPipeline(torch.nn.Module):
          # pin reference
         clip_encoder = self.root_pipeline.clip_encoder
         # render gaussian and keep features only
-        rgb_img, _, _, _ = self.root_pipeline(Rs, ts,
+        rgb_img, _, _, _ = self.root_pipeline(Rs, ts, opa_scaling=opa_scaling,
                                               pipeline_params=pipeline_params,
                                               override_color=override_color) #(B,H,W,3)
 

root_pipeline.py
@@ -200,7 +200,7 @@ class RootPipeline(torch.nn.Module):
                 shs = shs.float(),
                 colors_precomp = colors_precomp,
                 language_feature_precomp = language_feature_precomp.float(),
-                opacities = opacity.float(),
+                opacities = gaussian.get_opacity *  opacity.float(),
                 scales = scales.float(),
                 rotations = rotations.float(),
                 cov3D_precomp = cov3D_precomp)